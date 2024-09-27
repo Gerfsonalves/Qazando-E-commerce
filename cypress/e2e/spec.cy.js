89ad7cf1a19a0db89c1c8e0975b6091590eb2db0
Gerfson Alves <gerfson17@hotmail.com>
2024-09-27 17:20:42 -0300

    spec navegação

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -41,4 +41,28 @@ describe('template spec', () => {
       .should('have.text', 'Login realizado')
   })
 
+  it('Navegação no menu icon', () => {
+    cy.get(':nth-child(4) > .offcanvas-toggle > .fa').click()
+    cy.get(':nth-child(2) > #menuShopMenu').click()
+    cy.get(':nth-child(2) > :nth-child(1) > .mobile-sub-menu > :nth-child(1) > a').click()
+
+    cy.get(':nth-child(4) > .offcanvas-toggle > .fa').click()
+    cy.get(':nth-child(2) > #menuShopMenu').click()
+    cy.get(':nth-child(2) > :nth-child(1) > .mobile-sub-menu > :nth-child(2) > a').click()
+
+    cy.get(':nth-child(4) > .offcanvas-toggle > .fa').click()
+    cy.get(':nth-child(2) > #menuShopMenu').click()
+    cy.get(':nth-child(2) > :nth-child(1) > .mobile-sub-menu > :nth-child(3) > a').click()
+
+    cy.get(':nth-child(4) > .offcanvas-toggle > .fa').click()
+    cy.get(':nth-child(2) > #menuShopMenu').click()
+    cy.get(':nth-child(2) > :nth-child(1) > .mobile-sub-menu > :nth-child(4) > a').click()
+
+    cy.get(':nth-child(4) > .offcanvas-toggle > .fa').click()
+    cy.get(':nth-child(2) > #menuShopMenu').click()
+    cy.get(':nth-child(2) > :nth-child(1) > .mobile-sub-menu > :nth-child(5) > a').click()
+
+    cy.get('.logo > img').should('be.visible')
+  })
+
 })
